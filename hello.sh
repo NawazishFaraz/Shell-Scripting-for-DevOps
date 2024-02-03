da7a3e3dc4b063ed207221c503ade1943c49341e
Ubuntu <ubuntu@ip-172-31-43-192.ap-south-1.compute.internal>
2024-02-03 10:00:09 +0000

    first commit

---
+++ hello.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# this script is for beginners who wants to learn shell scripting from basics
+
+echo " hey everyone I am Nawazish Faraz "
+
+echo  " I am an aspiring DevOps Engineer from India "
+
+echo " you can connect me through my social profile for further details"
+
+echo " thank you | "
